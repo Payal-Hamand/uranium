54dd64dd7475370b8b259943282e8a954d7a6133
Payal-Hamand <payalhamand@gmail>
2022-04-08 13:13:57 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,31 +1,59 @@
 const express = require('express');
-const logger = require("../logger/logger");
-const helper = require("../util/helper");
-const formatter = require("../validator/formatter");
-const lodash= require('lodash');
 const router = express.Router();
 
-router.get('/hello', function (req, res) {
-    const arr =['january', 'february', 'march', 'april','may','june','july','august','september','octomber','november','december']
-const odd =([1,3,5,7,9,11,13,15,17,19])
-     console.log(lodash.chunk(arr,3));
-    var a =lodash.tail(odd)
-    console.log(a);
-});
-router.get('/test-me', function (req, res) {
-     res.send('My first ever api!  ')
+
+
+    router.get('/movies', function (req, res) {
+    const movi =['Khiladi','Lagan','Players','Golmaal','Dhamal']
+
    
+    res.send(movi)
 });
-router.get('/test-me1', function (req, res) {
-    res.send('My first ever api! ')
-    logger.wel()
-    helper.printD()
-    formatter.tr()
+
+const fil = [ {
+    "id": 1,
+    "name": "The Shining"
+   }, {
+    "id": 2,
+    "name": "Incendies"
+   }, {
+    "id": 3,
+    "name": "Rang de Basanti"
+   }, {
+    "id": 4,
+    "name": "Finding Nemo"
+   }]
+   
+router.get('/films',function(req,res){
+   
+    res.send(fil)
+
+});
+
+router.get('/films/:filmId',function(req,res){
+    const b = req.params.filmId
+    if(b < fil.length && b>=0){
+        res.send(fil[b])
+    }
+        else{
+            res.send('Use a Valid Index')
+        }
+
+});
 
 
-    formatter.UC() 
-    
+router.get('/movies/:indexNumber', function (req, res) {
+    const movi =['Khiladi','Lagan','Players','Golmaal','Dhamal']
+   const a = req.params.indexNumber
+   if(a < movi.length && a>=0){
+       res.send(movi[a])
+   }
+       else{
+           res.send('Use a Valid Index')
+       }
 });
+
+
 
 module.exports = router;
 // adding this comment for no reason
