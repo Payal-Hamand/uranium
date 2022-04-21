13be441ed63c330a39b94d1f9f22aeb1c9f47e6b
Payal-Hamand <payalhamand@gmail>
2022-04-21 19:06:55 +0530

    assignment 20/04/2022

---
+++ src/middlewares/middleware.js
@@ -0,0 +1,15 @@
+
+
+const headerCheck = async (req, res, next) => {
+    const header = req.headers.isfreeappuser
+    if(header){
+        if(header === "true")
+        req['isFreeAppUser'] = true
+        if(header === "false")
+        req['isFreeAppUser'] = false
+    next()
+    }
+    else res.send({msg: "An usefull header is missing"})
+}
+
+module.exports = {headerCheck}
