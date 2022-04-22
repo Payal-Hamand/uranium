f2bf4e009d89fa4d1f4b44d5773df381f0911600
Payal-Hamand <payalhamand@gmail>
2022-04-22 13:09:52 +0530

    Assignment 21/04/22

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -12,9 +12,21 @@ const userSchema = new mongoose.Schema( {
     password: String,
     gender: {
         type: String,
-        enum: ["male", "female", "other"]
+        enum: ["male","female","other"]
+    },
+    isDelete :{
+        type:Boolean,
+        default:false
     },
     age: Number,
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('Usermake', userSchema)
+
+
+
+
+//Token
+
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MjYxNjMwMDE3M2YyNWMxNDY0NzdkMDUiLCJiYXRjaCI6InRob3JpdW0iLCJvcmdhbmlzYXRpb24iOiJGVW5jdGlvblVwIiwiaWF0IjoxNjUwNTQ5OTk5fQ.huRITq-O2ikCfXtCj737zso-UOc9XmkBE_Y1v2UYb0M"
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MjYxNjMwMDE3M2YyNWMxNDY0NzdkMDUiLCJiYXRjaCI6InVyYW5pdW0iLCJvcmdhbmlzYXRpb24iOiJGVW5jdGlvblVwIiwiaWF0IjoxNjUwNTU3NjYxfQ.wjOd750JsDY0aCzYT0uWb8uMduhCwmbiFS0dUlxPSF0
